004b9fdfaaea09a083f32189d9e67e024ae181de
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-30 05:21:07 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,20 +20,20 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args = {})
+ 
+
+  # movie = Movie.create
+  # movie.title = args[:title]
+  # movie.release_date = args[:release_date]
+
+    title = args[:title]
+    release_date = args[:release_date]
+
   binding.pry
-
-   movie = Movie.create
-   movie.title = args[:title]
-   movie.release_date = args[:release_date]
-
-  #   title = args[:title]
-  #   release_date = args[:release_date]
-
-  # binding.pry
-  #   Movie.create do |m|
-  #     m.title = title.value
-  #     m.release_date = release_date.value
-  # end
+    Movie.create do |m|
+      m.title = title.value
+      m.release_date = release_date.value
+  end
 end
 
 def can_get_the_first_item_in_the_database
