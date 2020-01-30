df3b23fa4e8ddc0722b7a501b8265360b9a62d19
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-30 05:09:23 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,8 +22,8 @@ end
 def can_be_created_in_a_block(args = {})
 
     :title => args[:title]
-    :release_date = args[:release_date]
-.
+    :release_date =. args[:release_date]
+
     Movie.create do |m|
     m.title = title
     m.release_date = release_date
