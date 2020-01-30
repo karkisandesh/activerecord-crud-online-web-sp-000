e7c4c4191bd397f73f6b0ffb397b109f1917f543
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-30 02:27:10 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,8 +24,7 @@ def can_be_created_in_a_block(args = nil)
   # title == "Home Alone"
   # release_date == 1990
   
-  movie = Movie.new(args)
-  movie.tap do |m|
+ Movie.tap do |m|
     m.title = "Home Alone"
     m.release_date = 1990
   end
