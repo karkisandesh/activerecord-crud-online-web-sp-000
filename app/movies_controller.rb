e465a7eb73ce4deab633e157f3d82cc725fb7da3
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-30 05:10:44 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -19,7 +19,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = "nil")
+def can_be_created_in_a_block(args = {})
 
     title = args[:title]
     release_date = args[:release_date]
